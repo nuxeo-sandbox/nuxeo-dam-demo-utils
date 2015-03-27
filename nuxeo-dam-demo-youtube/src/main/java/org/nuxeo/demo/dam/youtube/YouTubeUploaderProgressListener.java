9dcde960a92c9f06dbd78d28d65022d1f0758e3e
Michaël Vachette <mvachette@nuxeo.com>
2015-03-27 18:26:32 +0100

    Removed Youtube plugin

--- nuxeo-dam-demo-youtube/src/main/java/org/nuxeo/demo/dam/youtube/YouTubeUploaderProgressListener.java
+++
@@ -1,8 +0,0 @@
-package org.nuxeo.demo.dam.youtube;
-
-public interface YouTubeUploaderProgressListener {
-    void onStart();
-    void onProgress(double progress);
-    void onComplete();
-    void onError();
-}
